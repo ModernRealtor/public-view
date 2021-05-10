41c1be91a9930e8e9eeac58bb41734d3980d38af
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-05-10 16:55:30 -0400

    Fixed lighthouse report for accesibility. Now 100

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function err_404() {
+  return (
+    <h1>
+      404 Not Found
+    </h1>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Helmet>
+        <html lang="en" />
+        <meta charSet="utf-8" />
+        <title>West-100</title>
+      </Helmet>
+      Hello world!
+    </div>
+  )
 }
