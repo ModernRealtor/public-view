5bea6cf85f2f6a842ce5e10f3f83bbefa343124d
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-05-11 19:01:32 -0400

    trying to make it bare minimum pwa. added icons and libraries

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,19 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Public View`,
+        short_name: `PV`,
+        start_url: `/`,
+        icon: `static/icon.png`,
+        background_color: `#FEFEFF`,
+        theme_color: `#FBB33B`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-offline`
   ],
 }
