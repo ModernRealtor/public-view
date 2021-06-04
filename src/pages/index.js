84f43f1237fe684f8dc82a1cc793d1cae3728b5b
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-04 16:42:27 -0400

    starting to add structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,21 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import Header from "../components/header"
+import Footer from "../components/footer"
+import Content from "./inner/landing"
 
 export default function Home() {
   return (
     <div>
       <Helmet>
-        <html lang="en" />
+        <html lang="en" class="bg-gray-50"/>
         <meta charSet="utf-8" />
         <meta name="description" content="public site" />
         <title>West-100</title>
       </Helmet>
-      Hello world!
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   )
 }
