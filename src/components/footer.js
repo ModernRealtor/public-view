6fbf1736c7b9ca3fdbb34b490617ee36486f9be6
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-04 20:29:13 -0400

    implimented responsiveness to the base layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Footer(props) {
   return (
-    <div class="bg-blue-100 pt-5">
-      <div class="border border-yellow-700 h-96">Footer</div>
+    <div class="bg-blue-100 pt-5 desktop:pb-5">
+      <div class="border border-yellow-700 h-96 desktop:mb-5">Footer</div>
     </div>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import Logo from "./logo"
 
 export default function Header(props) {
   return (
-    <div class="bg-green-100 flex p-2">
+    <div class="bg-green-100 tablet:bg-green-300 desktop:bg-green-900  flex p-2">
       <Logo />
     </div>
   )
