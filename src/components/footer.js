a41a5b097e859f3158e43703d8aca8b82e9abe01
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-04 20:00:00 -0400

    finished base layout for mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer(props) {
   return (
     <div class="bg-blue-100 pt-5">
-      <div class="border border-yellow-700 h-48">Footer</div>
+      <div class="border border-yellow-700 h-96">Footer</div>
     </div>
   )
 }
--- src/pages/inner/landing.js
+++ src/pages/inner/landing.js
@@ -2,10 +2,10 @@ import React from "react"
 
 export default function LandingPage(props) {
   return (
-    <div class="bg-red-100 flex flex-col gap-5 py-5">
+    <div class="bg-red-100 flex flex-col gap-5 py-5 my-5">
       <div class="border border-yellow-700 h-48">Hero Img</div>
-      <div class="border border-yellow-700 h-60">Hero Txt</div>
-      <div class="border border-yellow-700 h-48">Mailing List Form</div>
+      <div class="border border-yellow-700 h-60 mx-5">Hero Txt</div>
+      <div class="border border-yellow-700 h-48 mx-5">Mailing List Form</div>
     </div>
   )
 }
