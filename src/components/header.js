7070ea8ac5c4d5fbc6e3fd175e6001a65010bc17
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-07 13:08:11 -0400

    added menu button

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Logo from "./logo"
+import Menu from "./menu"
 
 export default function Header(props) {
   return (
-    <div class="bg-green-100 tablet:bg-green-300 desktop:bg-green-900  flex p-2">
+    <div class="bg-green-100 tablet:bg-green-300 desktop:bg-green-900 mx-auto flex p-3 justify-between">
       <Logo />
+      <Menu/>
     </div>
   )
 }
---
+++ src/components/menu.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Menu(props) {
+  return (
+    <div class="w-11 h-11 border border-orange">
+        Menu
+    </div>
+  )
+}
