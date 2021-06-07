8b822a97acf298de558120ce18f673b711e5edc9
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-07 12:55:58 -0400

    added new color pallette

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Logo(props) {
-  return <div class="mx-auto w-10 h-10 border-yellow-700 border">Logo</div>
+  return <div class="mx-auto w-10 h-10 bg-orange">Logo</div>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
@@ -8,6 +10,22 @@ module.exports = {
 
       desktop: "1280px",
       // => @media (min-width: 1280px) { ... }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      green: colors.emerald,
+      orange: {
+        light: '##fdd086',
+        DEFAULT: '#FBB33B',
+        dark: '#e59105',
+      }
     },
     extend: {},
   },
