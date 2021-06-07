7daac2f4501797e10ce6a34a82338eb474b543d3
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-07 11:39:18 -0400

    extracted hero image and hero text into its own component

---
+++ src/components/mailingList.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function MailingList(props) {
+  return (
+    <div class="border border-yellow-700 h-48 mx-5">Mailing List Form</div>
+  )
+}
--- src/pages/inner/landing.js
+++ src/pages/inner/landing.js
@@ -1,13 +1,12 @@
 import React from "react"
+import Hero from "../../components/hero"
+import MailingList from "../../components/mailingList"
 
 export default function LandingPage(props) {
   return (
     <div class="bg-red-100 flex flex-col gap-5 py-5 my-5">
-      <div class="border border-yellow-700 h-48 tablet:h-64">Hero Img</div>
-      <div class="border border-yellow-700 h-60 mx-5 tablet:h-72 tablet:w-2/3 desktop:w-auto">
-        Hero Txt
-      </div>
-      <div class="border border-yellow-700 h-48 mx-5">Mailing List Form</div>
+      <Hero/>
+      <MailingList/>
     </div>
   )
 }
