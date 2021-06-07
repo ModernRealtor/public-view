9828265964c6c35570112b77b8877c5b0190d8b0
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-07 14:13:42 -0400

    disable scroll for when menu is open

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,9 @@ import Menu from "./menu"
 
 export default function Header(props) {
   return (
-    <div class="bg-green-100 tablet:bg-green-300 desktop:bg-green-900 mx-auto flex p-3 justify-between">
+    <nav class="bg-green-100 tablet:bg-green-300 desktop:bg-green-900 mx-auto flex p-3 justify-between isolate">
       <Logo />
       <Menu/>
-    </div>
+    </nav>
   )
 }
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,9 +1,29 @@
-import React from "react"
+import React, {useState} from "react"
+
+function disableScroll(){
+    document.body.setAttribute("style", "overflow: hidden;");
+}
+
+function enableScroll(){
+    document.body.setAttribute("style", "overflow: auto;");
+}
 
 export default function Menu(props) {
-  return (
-    <div class="w-11 h-11 border border-orange">
-        Menu
-    </div>
-  )
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div>
+            <button class="w-11 h-11 border border-orange" 
+                onClick={() => {
+                    isOpen? enableScroll() : disableScroll()
+                    setIsOpen(!isOpen)
+                    }}>
+                {isOpen? "Close" : "Open"}
+            </button>
+            <div class="w-screen h-screen fixed left-0 pt-6 bg-indigo-100">
+                <button>Login</button>
+                <button>Contact</button>
+            </div>
+        </div>
+    )
 }
