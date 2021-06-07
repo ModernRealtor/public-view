f9149995c0101643afc9ffe551485d86387a7a18
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-07 17:55:26 -0400

    formatted the menu list and also removed bottom padding from footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Footer(props) {
   return (
-    <div className="pt-5 desktop:pb-5">
-      <div className="bg-gray-300 shadow-inner h-96 desktop:mb-5">Footer</div>
+    <div className="pt-5">
+      <div className="bg-gray-300 shadow-inner h-96">Footer</div>
     </div>
   )
 }
--- src/pages/inner/landing.js
+++ src/pages/inner/landing.js
@@ -4,7 +4,7 @@ import MailingList from "../../components/mailingList"
 
 export default function LandingPage(props) {
   return (
-    <div className="bg-indigo-200 flex flex-col gap-16 mb-5">
+    <div className="flex flex-col gap-16 mb-5">
       <Hero/>
       <MailingList/>
     </div>
