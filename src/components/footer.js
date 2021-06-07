a463bc157e1975d179d0ec288ea89917832cc2a9
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-07 12:04:40 -0400

    removed temp colors from footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Footer(props) {
   return (
-    <div class="bg-blue-100 pt-5 desktop:pb-5">
-      <div class="border border-yellow-700 h-96 desktop:mb-5">Footer</div>
+    <div class="pt-5 desktop:pb-5">
+      <div class="bg-gray-300 shadow-inner h-96 desktop:mb-5">Footer</div>
     </div>
   )
 }
