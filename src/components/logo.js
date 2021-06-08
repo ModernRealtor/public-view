a757b23d1b3e8b662d9fc6596a77595667956109
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-08 13:46:52 -0400

    added in the logo to the landing page

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,9 +1,12 @@
 import React from "react"
+import Icon from "../assets/icons/logo.svg"
 
 export default function Logo(props) {
   return (
     <div className={`${props.className} flex flex-nowrap`}>
-      <button className="w-11 h-11 bg-orange">Logo</button>
+      <button className="w-11 h-11">
+        <img src={Icon} alt="logo" />
+      </button>
       <div className="h-full flex flex-col place-content-center ml-3">
         Logo TxT
       </div>
