91623168743f3c13aa8d8af07c62e3741d1125a3
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-08 14:09:12 -0400

    added text to the logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -7,8 +7,13 @@ export default function Logo(props) {
       <button className="w-11 h-11">
         <img src={Icon} alt="logo" />
       </button>
-      <div className="h-full flex flex-col place-content-center ml-3">
-        Logo TxT
+      <div className="h-full flex flex-row flex-wrap place-content-center ml-1">
+        <span className="text-lg font-bold">
+          West-100
+        </span>
+        <span className="hidden text-sm leading-loose ml-3 tablet:inline-block">
+          Capital Reality Inc., Brokerage
+        </span>
       </div>
     </div>
   )
