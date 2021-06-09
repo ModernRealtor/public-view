b6defa2ff5b7350f10939d5e333325815d15ace7
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-09 14:55:42 -0400

    added dark button to menu and fixed responsiveness of the logo text

--- src/components/logo.js
+++ src/components/logo.js
@@ -64,12 +64,11 @@ export default function Logo(props) {
   return (
     <div className={`${props.className} flex flex-nowrap`}>
       <button className="w-11 h-11">
-        {/* <img src={Icon} alt="logo" /> */}
         <LogoIcon />
       </button>
-      <div className="h-full flex flex-row flex-wrap place-content-center ml-1">
-        <span className="text-lg font-bold">West-100</span>
-        <span className="hidden text-sm leading-loose ml-3 tablet:inline-block">
+      <div className="h-full flex flex-row flex-wrap place-content-center ml-1 justify-start">
+        <span className="text-lg font-bold tablet:mr-3 ">West-100</span>
+        <span className="hidden text-sm leading-loose tablet:inline-block">
           Capital Reality Inc., Brokerage
         </span>
       </div>
--- src/components/menu.js
+++ src/components/menu.js
@@ -63,13 +63,16 @@ export default function Menu(props) {
       <div
         className={`z-50 bg-white w-11/12 h-screen ${
           isOpen ? "fixed" : "hidden"
-        } flex-grow flex-col-reverse flex-wrap right-0 mt-11 pt-5 tablet:w-auto tablet:h-auto tablet:m-0 tablet:p-0 tablet:flex tablet:static tablet:flex-row-reverse tablet:flex-nowrap tablet:justify-start tablet:bg-transparent gap-10 isolate`}
+        } flex-grow flex-col-reverse flex-wrap right-0 mt-11 pt-5 tablet:w-auto tablet:h-auto tablet:m-0 tablet:p-0 tablet:flex tablet:static tablet:flex-row-reverse tablet:flex-nowrap tablet:justify-start tablet:bg-transparent gap-5 desktop:gap-10 isolate`}
       >
         <button className="border border-yellow-700 w-full text-left py-3 px-5 mt-5 tablet:w-auto tablet:text-center tablet:py-0 tablet:my-0">
-          Contact
+          Get Started
         </button>
         <button className="border border-yellow-700 w-full text-left py-3 px-5 mt-5 tablet:w-auto tablet:text-center tablet:py-0 tablet:my-0">
           Login
+        </button>
+        <button className="border border-yellow-700 w-full text-left py-3 px-5 mt-5 tablet:w-auto tablet:text-center tablet:py-0 tablet:my-0">
+          Dark
         </button>
       </div>
     </div>
