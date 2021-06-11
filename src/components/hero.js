56345d9f7ce51d8e4d6af72fcdaf2a13427304e2
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-11 12:58:25 -0400

    modify hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -27,8 +27,8 @@ function HeroImage(props) {
 function HeroText(props) {
   return (
     <div className="z-10 w-full aspect-w-1 aspect-h-1 tablet:aspect-w-16 tablet:aspect-h-9 !absolute bg-indigo-100 bg-opacity-50">
-      <div className="flex flex-col p-10 tablet:gap-5 laptop:flex-row">
-        <h1 className="flex-grow block text-4xl tablet:text-5xl tablet:gap-16 capitalize font-black text-white filter drop-shadow flex flex-col justify-around bg-green-100 bg-opacity-50">
+      <div className="flex flex-col p-10 tablet:flex-row bg-yellow-200 bg-opacity-50">
+        <h1 className="flex-grow text-4xl tablet:text-5xl capitalize font-black text-white filter drop-shadow flex flex-col justify-between bg-green-100 bg-opacity-50">
           <span className="border border-black">Your next</span>
           <span className="border border-black">home</span>
           <span className="flex flex-col relative ">
@@ -36,7 +36,7 @@ function HeroText(props) {
             <span className="border border-black">click away.</span>
           </span>
         </h1>
-        <div className="self-end tablet:absolute tablet:bottom-10 laptop:static laptop:flex-grow">
+        <div className="self-end tablet:flex-grow bg-gray-100 bg-opacity-50">
           <button className="px-5 py-3 bg-white ">Get Started</button>
         </div>
       </div>
