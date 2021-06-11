309acf4c86e7f9fd405ccc595ff4959734d673f1
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-11 18:10:11 -0400

    made menu items come from a data object

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,17 @@
 import React, { useState } from "react"
 import { MenuAlt3Icon, XIcon } from "@heroicons/react/solid"
 
+const menuData = {
+  entries: [
+    {
+      title: "Get Started",
+    },
+    {
+      title: "Login",
+    },
+  ],
+}
+
 export default function Menu(props) {
   const [isOpen, setIsOpen] = useState(false)
 
@@ -20,19 +31,15 @@ export default function Menu(props) {
         )}
       </button>
       <div
-        className={`z-50 bg-white w-11/12 h-screen ${
+        className={`z-50 bg-white w-full h-screen ${
           isOpen ? "fixed" : "hidden"
         } flex-grow flex-col-reverse flex-wrap right-0 mt-11 pt-5 tablet:w-auto tablet:h-auto tablet:m-0 tablet:p-0 tablet:flex tablet:static tablet:flex-row-reverse tablet:flex-nowrap tablet:justify-start tablet:bg-transparent gap-5 laptop:gap-10 isolate`}
       >
-        <button className="border border-yellow-700 w-full text-left py-3 px-5 mt-5 tablet:w-auto tablet:text-center tablet:py-0 tablet:my-0">
-          Get Started
-        </button>
-        <button className="border border-yellow-700 w-full text-left py-3 px-5 mt-5 tablet:w-auto tablet:text-center tablet:py-0 tablet:my-0">
-          Login
-        </button>
-        <button className="border border-yellow-700 w-full text-left py-3 px-5 mt-5 tablet:w-auto tablet:text-center tablet:py-0 tablet:my-0">
-          Dark
-        </button>
+        {menuData.entries.map(item => (
+          <button className="border border-yellow-700 w-full text-left py-3 px-5 mt-5 tablet:w-auto tablet:text-center tablet:py-0 tablet:my-0">
+            {item.title}
+          </button>
+        ))}
       </div>
     </div>
   )
