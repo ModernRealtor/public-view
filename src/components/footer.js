079337e0f26783b6f6acadc11a6f16065a953836
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-11 19:00:04 -0400

    fixed console errors about keys and also made prefreneces display language and theme dynamically

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { ChevronUpIcon } from "@heroicons/react/solid"
 import { Disclosure } from "@headlessui/react"
 
@@ -23,6 +23,7 @@ const footerData = {
   },
   socials: {
     title: "Stay Connected",
+    description: "Follow us on social media!",
     entries: [
       {
         title: "Facebook",
@@ -44,13 +45,52 @@ const footerData = {
       },
     ],
   },
-  copyright: "copyright text",
-  language: [
-    {
-      title: "English",
-      value: "en",
-    },
-  ],
+  preferences: {
+    title: "Preferences",
+    description: "Modify your preferences",
+    entries: [
+      {
+        key: "language",
+        tag: LanguageSelector,
+      },
+      {
+        key: "theme",
+        tag: ThemeSelector,
+      },
+    ],
+  },
+  language: {
+    title: "language",
+    description: "Choose a language.",
+    entries: [
+      {
+        title: "English",
+        value: "en",
+      },
+    ],
+  },
+  theme: {
+    title: "Theme",
+    description: "Choose a theme.",
+    entries: [
+      {
+        title: "Light",
+        value: "l",
+      },
+      {
+        title: "Dark",
+        value: "d",
+      },
+      {
+        title: "System",
+        value: "sys",
+      },
+    ],
+  },
+  copyright: {
+    title: "Copyright",
+    value: "this is the copyright",
+  },
   affiliations: [
     {
       icon: null,
@@ -83,7 +123,7 @@ function ContactUs(props) {
           <Disclosure.Panel className="text-gray-500">
             {props.description}
             {props.entries.map(item => (
-              <div>
+              <div key={item.short}>
                 {item.short}: {item.value}
               </div>
             ))}
@@ -94,11 +134,57 @@ function ContactUs(props) {
   )
 }
 
+function LanguageSelector(props) {
+  return <div>Language</div>
+}
+
+function ThemeSelector(props) {
+  return <div>Choose Theme</div>
+}
+
+function Preferences(props) {
+  return (
+    <Disclosure>
+      {({ open }) => (
+        <>
+          <Disclosure.Button className="py-2  w-full flex justify-between">
+            <span>{props.title}</span>
+            <ChevronUpIcon
+              className={`${
+                open ? "transform rotate-180" : ""
+              } w-5 h-5 text-purple-500`}
+            />
+          </Disclosure.Button>
+          <Disclosure.Panel className="text-gray-500">
+            {props.description}
+            {props.entries.map(item => {
+              return <item.tag key={item.key} {...footerData[item.key]} />
+            })}
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
+
+function Socials(props) {
+  return <div className="border border-black">Socials</div>
+}
+
+function Copyright(props) {
+  return <div>{props.value}</div>
+}
+
 export default function Footer(props) {
   return (
-    <div className="bg-gray-300 shadow-inner h-96 mt-16 p-10">
+    <div className="bg-gray-300 shadow-inner mt-16 p-10">
       <div className="border border-red-300 h-full">
         <ContactUs {...footerData.contact} />
+        <Preferences {...footerData.preferences} />
+        <hr className="my-10" />
+        <Socials {...footerData.socials} />
+        <hr className="my-10" />
+        <Copyright {...footerData.copyright} />
       </div>
     </div>
   )
--- src/components/menu.js
+++ src/components/menu.js
@@ -36,7 +36,10 @@ export default function Menu(props) {
         } flex-grow flex-col-reverse flex-wrap right-0 mt-11 pt-5 tablet:w-auto tablet:h-auto tablet:m-0 tablet:p-0 tablet:flex tablet:static tablet:flex-row-reverse tablet:flex-nowrap tablet:justify-start tablet:bg-transparent gap-5 laptop:gap-10 isolate`}
       >
         {menuData.entries.map(item => (
-          <button className="border border-yellow-700 w-full text-left py-3 px-5 mt-5 tablet:w-auto tablet:text-center tablet:py-0 tablet:my-0">
+          <button
+            key={item.title}
+            className="border border-yellow-700 w-full text-left py-3 px-5 mt-5 tablet:w-auto tablet:text-center tablet:py-0 tablet:my-0"
+          >
             {item.title}
           </button>
         ))}
