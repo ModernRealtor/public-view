d56755493b0eaf2a76de284ee333405ac839220c
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-11 17:14:53 -0400

    replaced svg function for the icons with importing the library

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,50 +1,5 @@
 import React, { useState } from "react"
-
-function MenuIcon(props) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      aria-labelledby="title"
-      className="h-6 w-6 m-auto"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <title id="title" lang="en">
-        Open Menu
-      </title>
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M4 6h16M4 12h16m-7 6h7"
-      />
-    </svg>
-  )
-}
-
-function CloseIcon(props) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      aria-labelledby="title"
-      className="h-6 w-6 m-auto"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <title id="title" lang="en">
-        Close Menu
-      </title>
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M6 18L18 6M6 6l12 12"
-      />
-    </svg>
-  )
-}
+import {MenuAlt3Icon, XIcon} from '@heroicons/react/solid'
 
 export default function Menu(props) {
   const [isOpen, setIsOpen] = useState(false)
@@ -58,7 +13,7 @@ export default function Menu(props) {
           setIsOpen(!isOpen)
         }}
       >
-        {isOpen ? <CloseIcon /> : <MenuIcon />}
+        {isOpen ? <XIcon className="h-6 w-6 m-auto" /> : <MenuAlt3Icon className="h-6 w-6 m-auto" />}
       </button>
       <div
         className={`z-50 bg-white w-11/12 h-screen ${
