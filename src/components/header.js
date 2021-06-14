0158138e0a84a475d76bd45358970e385acdd029
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-14 13:54:54 -0400

    added shadow to the top menu

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import Menu from "./menu"
 
 export default function Header(props) {
   return (
-    <nav className="sticky top-0 z-30 flex justify-between p-5 mx-auto bg-white">
+    <nav className="sticky top-0 z-30 flex justify-between p-5 mx-auto bg-white shadow">
       <Logo className="flex-auto w-1/3" />
       <Menu className="flex-auto w-1/3" />
     </nav>
