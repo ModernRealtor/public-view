d5c15495f8a0064e517f66bf63d32f4b535d8687
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-14 13:26:46 -0400

    made the top nave bar sticky for scroll

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import Menu from "./menu"
 
 export default function Header(props) {
   return (
-    <nav className="flex justify-between p-5 mx-auto bg-white">
+    <nav className="sticky top-0 z-50 flex justify-between p-5 mx-auto bg-white">
       <Logo className="flex-auto w-1/3" />
       <Menu className="flex-auto w-1/3" />
     </nav>
