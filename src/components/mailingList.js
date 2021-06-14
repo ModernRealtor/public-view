117a642953ebd1cd18eaf2e83e34885d83971ee2
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-06-14 15:36:34 -0400

    remove unused svg from mailing list section that was left by accident

--- src/components/mailingList.js
+++ src/components/mailingList.js
@@ -1,14 +1,7 @@
 import React from "react"
 
-import MLSIcon from "../assets/icons/mls"
-import RealtorIcon from "../assets/icons/realtor"
-
 export default function MailingList(props) {
   return (
-    <div className="h-48 mx-5 border border-yellow-700">
-      Mailing List Form
-      <MLSIcon />
-      <RealtorIcon />
-    </div>
+    <div className="h-48 mx-5 border border-yellow-700">Mailing List Form</div>
   )
 }
