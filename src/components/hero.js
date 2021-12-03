8c8351762136f8b09bc039c6e9c7350c564b3568
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-12-02 20:17:04 -0500

    add some notes to myself also made text align different responsively

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,9 +8,9 @@ export default function Hero(props) {
   return (
     <div className={`${props.className} pb-10 tablet:pt-10 laptop:py-24 desktop:py-32`}>
       <div className="flex flex-col tablet:flex-row">
-        <div className="pt-5 pb-10 desktop:pt-0">
-          <h1 className="font-bold py-5 text-4xl capitalize laptop:text-5xl laptop:pb-10 desktop:text-right desktop:text-6xl desktop:pb-14 desktop:pt-0">{heroTitle}</h1>
-          <p className="pb-4 desktop:text-right">{heroText}</p>
+        <div className="pt-5 pb-10 desktop:pt-0 text-center tablet:text-left desktop:text-right">
+          <h1 className="font-bold py-5 text-4xl capitalize laptop:text-5xl laptop:pb-10 desktop:text-6xl desktop:pb-14 desktop:pt-0">{heroTitle}</h1>
+          <p className="pb-4">{heroText}</p>
           <button className="bg-primary text-secondary hover:bg-accent px-4 py-2 desktop:float-right">Get Started</button>
         </div>
         <Undraw name="house_searching" primaryColor="" className="" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,13 @@ export default function Home() {
         <Hero className="outer-layout bg-secondary text-primary"/>
         <div className="h-96 outer-layout bg-primary text-secondary">
           <p>What we do/Value Proposition</p>
-          <p>Our Services (solutions: buy/sell/etc)</p>
+          <p>in the meantime can have like, metrics like list of, [number]+ [descriptor]</p>
+          <p>for ex: 100+ houses sold, 1000+ offers accepted, 10+ years in business, etc</p>
+          <p>in future will also have search bar to search properties in real time</p>
         </div>
         <div className="h-48 outer-layout bg-secondary text-primary">
           <p>Testimonials</p>
+          <p>get from server. at first dummu values, but in future will get from google business reviews</p>
         </div>
     </Layout>
   )
