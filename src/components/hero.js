4294c62e702f98f1d946aee3a5fb6062f1f431f1
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-12-02 19:55:40 -0500

    worked on the hero area. added undraw illustration.

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,14 +1,20 @@
 import React from "react"
+import Undraw from "react-undraw"
 
-let heroText = "Your next home is a click away."
-
+let heroTitle = "Your next home is a click away."
+let heroText = "Let us help you find the property of your dreams."
 
 export default function Hero(props) {
   return (
-    <div className={`${props.className}`}>
-      <h1 className="font-black text-4xl capitalize h-48">
-        {heroText}
-      </h1>
+    <div className={`${props.className} pb-10 tablet:pt-10 laptop:py-24 desktop:py-32`}>
+      <div className="flex flex-col tablet:flex-row">
+        <div className="pt-5 pb-10 desktop:pt-0">
+          <h1 className="font-bold py-5 text-4xl capitalize laptop:text-5xl laptop:pb-10 desktop:text-right desktop:text-6xl desktop:pb-14 desktop:pt-0">{heroTitle}</h1>
+          <p className="pb-4 desktop:text-right">{heroText}</p>
+          <button className="bg-primary text-secondary hover:bg-accent px-4 py-2 desktop:float-right">Get Started</button>
+        </div>
+        <Undraw name="house_searching" primaryColor="" className="" />
+      </div>
     </div>
   )
 }
