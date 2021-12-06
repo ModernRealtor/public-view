dd5af0f3d777738bb6a1c81cfdf8f189967a61f1
Diadem Shoukralla <diademshoukralla@gmail.com>
2021-12-06 18:37:20 -0500

    minor changes to style. started thinking about dynamic images and trreb data

--- src/components/listing.js
+++ src/components/listing.js
@@ -14,11 +14,11 @@ function BedIcon(props) {
             x="0px" 
             y="0px" 
             viewBox="0 0 122.88 78.63" 
-            style={{"enable-background":"new 0 0 122.88 78.63"}}
+            style={{"enableBackground":"new 0 0 122.88 78.63"}}
             className={props.className}
         >
             <g>
-                <path style={{"fill-rule":"evenodd","clip-rule":"evenodd"}} d="M3.36,0h7.3c1.85,0,3.36,1.56,3.36,3.36v43.77h37.33L61.99,9.69h41.85c10.47,0,19.04,8.59,19.04,19.04v19.04 h-0.02c0.01,0.12,0.02,0.24,0.02,0.37v30.49h-14.02V64.32H14.02v13.66H0V3.36C0,1.51,1.51,0,3.36,0L3.36,0z M35.44,10.37 c8.62,0,15.61,6.99,15.61,15.61c0,8.62-6.99,15.61-15.61,15.61c-8.62,0-15.61-6.99-15.61-15.61 C19.83,17.36,26.82,10.37,35.44,10.37L35.44,10.37z"/>
+                <path style={{"fillRule":"evenodd","clipRule":"evenodd"}} d="M3.36,0h7.3c1.85,0,3.36,1.56,3.36,3.36v43.77h37.33L61.99,9.69h41.85c10.47,0,19.04,8.59,19.04,19.04v19.04 h-0.02c0.01,0.12,0.02,0.24,0.02,0.37v30.49h-14.02V64.32H14.02v13.66H0V3.36C0,1.51,1.51,0,3.36,0L3.36,0z M35.44,10.37 c8.62,0,15.61,6.99,15.61,15.61c0,8.62-6.99,15.61-15.61,15.61c-8.62,0-15.61-6.99-15.61-15.61 C19.83,17.36,26.82,10.37,35.44,10.37L35.44,10.37z"/>
             </g>
         </svg>
     )
@@ -33,11 +33,11 @@ function BathIcon(props) {
             x="0px" 
             y="0px" 
             viewBox="0 0 89.19 122.88" 
-            style={{"enable-background":"new 0 0 89.19 122.88"}}
+            style={{"enableBackground":"new 0 0 89.19 122.88"}}
             className={props.className}
         >
         <g>
-            <path style={{"fill-rule":"evenodd","clip-rule":"evenodd"}} d="M10.37,84.59C4.17,79.12,0.46,71.46,0,60.91h89.19c-0.33,9.94-2.94,17.23-8.18,21.45 c-5.25,4.23-14.02,4.67-20.88,7.81c-1.49,0.68-2.87,1.52-4.12,2.56c-7.58,6.3-5.38,15.5,1.61,21.31v8.84H10.6v-15.91H2.71V94.84 h7.66V84.59L10.37,84.59z M0.03,54.56V0c6.73,0,13.47,0,20.2,0l6.95,54.56H0.03L0.03,54.56z M31.38,48.73h57.71v5.83H31.38V48.73 L31.38,48.73z"/>
+            <path style={{"fillRule":"evenodd","clipRule":"evenodd"}} d="M10.37,84.59C4.17,79.12,0.46,71.46,0,60.91h89.19c-0.33,9.94-2.94,17.23-8.18,21.45 c-5.25,4.23-14.02,4.67-20.88,7.81c-1.49,0.68-2.87,1.52-4.12,2.56c-7.58,6.3-5.38,15.5,1.61,21.31v8.84H10.6v-15.91H2.71V94.84 h7.66V84.59L10.37,84.59z M0.03,54.56V0c6.73,0,13.47,0,20.2,0l6.95,54.56H0.03L0.03,54.56z M31.38,48.73h57.71v5.83H31.38V48.73 L31.38,48.73z"/>
         </g>
         </svg>
     )
@@ -45,11 +45,10 @@ function BathIcon(props) {
 
 
 export function ListingCard(props) {
-    console.log(props)
     return (
         <div className="w-64 h-72 text-primary text-xs">
-            <div className="w-full h-3/5 rounded-t bg-yellow-200">Img Placeholder</div>
-            <div className="w-full h-2/5 gap-3 bg-secondary rounded-b p-3 flex flex-col justify-items-stretch">
+            <div className="w-full h-3/5 rounded-t-sm bg-gradient-to-t from-secondary to-primary "></div>
+            <div className="w-full h-2/5 gap-3 bg-secondary rounded-b-sm p-3 flex flex-col justify-items-stretch">
                 <div className="font-thin align-middle"><LocationMarkerIcon className="h-4 mr-2 -ml-0.5 float-left"/> {props.info.address}</div>
                 <div className="flex gap-4 justify-between">
                     <div className="flex content-center"><BedIcon className="w-4 self-center mr-2"/> {props.info.numBed} bed</div>
@@ -57,8 +56,8 @@ export function ListingCard(props) {
                     <div className="flex"><ArrowsExpandIcon className="h-4 self-center mr-2"/>{props.info.size} m <sup className="relative top-1">2</sup></div>
                 </div>
                 <div className="flex-grow flex">
-                    <button className="w-1/2 border border-primary">Book Now</button>
-                    <p className="w-1/2 text-center self-center text-base font-bold">${props.info.price.toLocaleString()}</p>
+                    <button className="w-2/5 text-sm font-normal rounded border-2 border-primary hover:border-accent hover:text-accent ">Book Now</button>
+                    <p className="w-3/5 text-center self-center text-base font-bold">${props.info.price.toLocaleString()}</p>
                 </div>
             </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -27,49 +27,62 @@ let houses = [
     numBed: 3,
     numBath: 2,
     size: 2000,
-    price: 120000
+    price: 1200000,
+    img: "house1.jpg"
   },
   {
     address: "44 McFake St., Guelph",
     numBed: 1,
     numBath: 1,
     size: 1400,
-    price: 800000
+    price: 800000,
+    img: "house2.jpg"
   },
   {
     address: "90 Faking Rd., London",
     numBed: 2,
     numBath: 2,
     size: 1800,
-    price: 134000
+    price: 1340000,
+    img: "house3.jpg"
+  },
+  {
+    address: "1100 Address Fake., Toronto",
+    numBed: 1,
+    numBath: 1,
+    size: 1000,
+    price: 1000000,
+    img: "house4.jpg"
   },
 ]
+
 
 export default function Home() {
   return (
     <Layout>
         <Hero className="outer-layout bg-secondary text-primary"/>
-        <div className="h-96 outer-layout bg-primary text-secondary">
-          <p>recent listings. maybe other properties you can book with brokerage</p>
-          <p>link to see all (can filter)</p>
-          <div className="flex">
+        <div className="outer-layout bg-half text-primary py-12">
+          <div className="uppercase text-xs">&#8212;&#8212; Recent</div>
+          <div className="flex justify-between mt-2 mb-20">
+            <h2 className="font-semibold text-3xl">Our Recent Listings</h2>
+            <a href="#" className="text-sm">Explore All &rarr;</a>
+          </div>
+          <div className="flex justify-between">
             {houses.map((info, i) => (
               <ListingCard key={i} info={info}/>
             ))}
           </div>
         </div>
-        <div className="h-96 outer-layout bg-secondary text-primary">
+        <div className="h-96 outer-layout bg-primary text-secondary">
           <p>Why work with us?</p>
           <p>list value offering to clients: knowledgeable, hardworking, industry network</p>
           <p>knowledgeable: realtor is essential for knowing market and value/etc. with XX years in business, we have the know how</p>
           <p>hardworking: Never rest until you're satisfied. Our client-centered approach will make you feel at ease in this competitive landscape</p>
           <p>industry network: gain access to top industry proffessionals that we've worked with throughout the years; such as attourneys, mortage brokers, inspectors, etc</p>
         </div>
-        <div className="h-96 outer-layout bg-primary text-secondary">
+        <div className="h-96 outer-layout bg-secondary text-primary">
           <p>buy section</p>
           <p>No matter what kind of property you dream of, we will help make that dream a realty</p>
-        </div>
-        <div className="h-96 outer-layout bg-secondary text-primary">
           <p>Sell section</p>
           <p>Selling a property is a daunting task. We promise to help you get the best price, according to market value</p>
         </div>
