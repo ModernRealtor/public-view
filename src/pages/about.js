c9c60e00fa0bc211168d150599f629a6dd4ce22f
Diadem Shoukralla <diademshoukralla@gmail.com>
2022-02-25 18:54:55 -0500

    added better styling to org about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,12 @@ import { contactIcons } from "../assets/icons/socials"
 import Layout from "../components/layout"
 
 export default function About({location}) {
-  let {cms: {org: {info: {about, tagline}, contact, team: {edges}}}} = useStaticQuery(graphql`
+  let {cms: {org: {info: {name, about, tagline}, contact, team: {edges}}}} = useStaticQuery(graphql`
   query {
     cms {
       org {
         info {
+          name
           about
           tagline
         }
@@ -45,10 +46,14 @@ export default function About({location}) {
   return (
     <Layout path={location.pathname} title="About Us">
       <div className="outer-layout">
-        <h1 className="font-semibold text-4xl pt-8">About Us</h1>
-        <div className="pb-6">{about || ""} {tagline || ""}</div>
-        <div className="py-10">
-          <h2 className="font-semibold text-3xl">Contact Us</h2>
+        <h1 className="font-semibold text-4xl py-4">About Us</h1>
+        <div className="pb-2">
+          <h2 className="text-2xl">{name}</h2>
+          {tagline ? <h3 className="text-lg py-1">{tagline}</h3> : ""}
+          {about ? <p className="py-6">{about}</p> : ""} 
+        </div>
+        <div className="py-4">
+          <h2 className="font-semibold text-3xl pb-4">Contact Us</h2>
           Reach us using any of the following:
           {Object.entries(contact)
             .filter(entry => entry[1])
