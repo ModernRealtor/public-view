8854810c166328dc8975d6f6cd67723cd09a9352
Diadem Shoukralla <diademshoukralla@gmail.com>
2022-02-28 19:46:55 -0500

    started implementing download of cloudinary images to use on site

---
+++ download-img.js
@@ -0,0 +1,17 @@
+let fs = require('fs')
+let fetch = require('node-fetch')
+let {promisify} = require('node:util')
+let {pipeline} = require('node:stream')
+
+const streamPipeline = promisify(pipeline)
+
+function downloadImage(url, outpath) {
+    return fetch(url).then(resp => {
+
+    }).catch(err => {
+        console.error(err)
+        return Promise.reject(new Error(`Error downloading image ${url} to ${outpath}`))
+    })
+}
+
+module.exports = downloadImage
