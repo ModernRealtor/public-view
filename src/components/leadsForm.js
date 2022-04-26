43167e8573acc87b803801a0c22734bb8eb97e2a
Diadem Shoukralla <diademshoukralla@gmail.com>
2022-04-26 18:39:00 -0400

    styled checkboxes for hover

--- src/components/leadsForm.js
+++ src/components/leadsForm.js
@@ -4,7 +4,7 @@ import {HouseSelect} from "../assets/icons/undraw"
 import {submitLeadForm} from "../services/gql-api"
 
 let inputClass = "form-input mt-1 block w-full rounded-md border-secondary-400 focus:border-primary-500 focus:ring focus:ring-primary-300 focus:ring-opacity-25 placeholder:font-thin placeholder:opacity-75 "
-let checkClass = "form-checkbox w-5 h-5 inline-block float-left mr-1 cursor-pointer checked:bg-primary-500 checked:focus:bg-primary-500 checked:focus:ring-1 checked:focus:ring-primary-500"
+let checkClass = "form-checkbox w-5 h-5 inline-block float-left mr-1 cursor-pointer checked:bg-primary-500 checked:focus:bg-primary-500 checked:focus:ring-1 checked:focus:ring-primary-500 hover:border-primary-500 checked:hover:bg-primary-500 checked:hover:ring checked:hover:ring-primary-300 checked:hover:ring-opacity-25"
 
 
 let LeadForm = ({className}) => {
@@ -39,16 +39,16 @@ let LeadForm = ({className}) => {
 
     let emailRequired = pref === "email";
     return (<div className={`${className}`}>
-        <div className="flex flex-col tablet:flex-row place-content-center">
-            <div className="flex-none flex-col-reverse place-content-start gap-4 tablet:basis-2/5 tablet:pr-12 tablet:flex">
+        <div className="flex flex-col tablet:flex-row place-content-center laptop:place-content-around laptop:gap-4">
+            <div className="flex-none flex-col-reverse place-content-start gap-4 tablet:basis-2/5 desktop:basis-1/3 tablet:pr-12 tablet:flex">
                 <div className="relative w-full overflow-clip tablet:overflow-visible" style={{paddingTop: "55%"}}>
                     <div className="absolute top-0">
                         <HouseSelect className="w-full h-fit"/>
                     </div>
                 </div>
-                <h2 className="text-primary-500 text-3xl font-semi pb-6">Ready to take the next step?</h2>
+                <h2 className="text-primary-500 text-3xl font-semi pb-6 desktop:text-4xl">Ready to take the next step?</h2>
             </div>
-            <div className="relative">
+            <div className="relative laptop:w-full">
                 <form onSubmit={submitForm} className={`${loading? "cursor-wait animate-pulse" : ""}`}>
                     <fieldset className={`flex flex-col gap-2 disabled:pointer-events-none`} disabled={loading}>
                         <div className="block">
