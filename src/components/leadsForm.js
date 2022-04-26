64263b0807eb8a2c6b1c4280d3b8c2033f65e90a
Diadem Shoukralla <diademshoukralla@gmail.com>
2022-04-26 19:17:42 -0400

    finished styling responsiveness of leads form

--- src/components/leadsForm.js
+++ src/components/leadsForm.js
@@ -38,7 +38,7 @@ let LeadForm = ({className}) => {
     }
 
     let emailRequired = pref === "email";
-    return (<div className={`${className}`}>
+    return (<section className={`${className}`}>
         <div className="flex flex-col tablet:flex-row place-content-center laptop:place-content-around laptop:gap-4">
             <div className="flex-none flex-col-reverse place-content-start gap-4 tablet:basis-2/5 desktop:basis-1/3 tablet:pr-12 tablet:flex">
                 <div className="relative w-full overflow-clip tablet:overflow-visible" style={{paddingTop: "55%"}}>
@@ -50,71 +50,75 @@ let LeadForm = ({className}) => {
             </div>
             <div className="relative laptop:w-full">
                 <form onSubmit={submitForm} className={`${loading? "cursor-wait animate-pulse" : ""}`}>
-                    <fieldset className={`flex flex-col gap-2 disabled:pointer-events-none`} disabled={loading}>
-                        <div className="block">
-                            <span className="font-medium cursor-default">Interested In</span>
-                            <div className="flex flex-wrap gap-x-6 justify-start">
-                                {Object.keys(clientPlans).map((type, i) => (
-                                <label key={i} className="cursor-pointer">
-                                    <input type="checkbox" value={type} checked={clientPlans[type]} onChange={({target: {value, checked}}) => {
-                                        let newState = Object.assign({}, clientPlans)
-                                        newState[value] = (checked === true)
-                                        setClientPlans(newState)
-                                    }} className={`${inputClass} ${checkClass}`}/>
-                                    <span className="capitalize align-middle">{type}</span>
-                                </label>
-                                ))}
-                            </div>                        
+                    <fieldset className={`flex flex-col gap-2 desktop:gap-4 disabled:pointer-events-none`} disabled={loading}>
+                        <div className="grid grid-cols-3 desktop:grid-flow-col gap-2">
+                            <div className="col-span-3 desktop:col-span-1">
+                                <span className="font-medium cursor-default">Interested In</span>
+                                <div className="flex flex-wrap gap-x-6 justify-start">
+                                    {Object.keys(clientPlans).map((type, i) => (
+                                    <label key={i} className="cursor-pointer">
+                                        <input type="checkbox" value={type} checked={clientPlans[type]} onChange={({target: {value, checked}}) => {
+                                            let newState = Object.assign({}, clientPlans)
+                                            newState[value] = (checked === true)
+                                            setClientPlans(newState)
+                                        }} className={`${inputClass} ${checkClass}`}/>
+                                        <span className="capitalize align-middle">{type}</span>
+                                    </label>
+                                    ))}
+                                </div>                        
+                            </div>
+                            <div className="col-span-3 desktop:col-span-1">
+                                <span className="font-medium cursor-default">Property Type</span>
+                                <div className="flex flex-wrap gap-x-6 justify-start">
+                                    {Object.keys(propTypes).map((type, i) => (
+                                    <label key={i} className="cursor-pointer">
+                                        <input type="checkbox" value={type} checked={propTypes[type]} onChange={({target: {value, checked}}) => {
+                                            let newState = Object.assign({}, propTypes)
+                                            newState[value] = (checked === true)
+                                            setPropTypes(newState)
+                                        }} className={`${inputClass} ${checkClass}`}/>
+                                        <span className="capitalize align-middle">{type}</span>
+                                    </label>
+                                    ))}
+                                </div>                        
+                            </div>
+                            <label className="block col-span-3 desktop:row-span-2 desktop:col-span-2">
+                                <span className="font-medium">Additional Comments</span>
+                                <textarea value={comments} onChange={({target: {value}}) => setComments(value)} rows={4} className={`${inputClass} form-textarea`} placeholder="Anything extra you would like us to know?"/>
+                            </label>
                         </div>
-                        <div className="block">
-                            <span className="font-medium cursor-default">Property Type</span>
-                            <div className="flex flex-wrap gap-x-6 justify-start">
-                                {Object.keys(propTypes).map((type, i) => (
-                                <label key={i} className="cursor-pointer">
-                                    <input type="checkbox" value={type} checked={propTypes[type]} onChange={({target: {value, checked}}) => {
-                                        let newState = Object.assign({}, propTypes)
-                                        newState[value] = (checked === true)
-                                        setPropTypes(newState)
-                                    }} className={`${inputClass} ${checkClass}`}/>
-                                    <span className="capitalize align-middle">{type}</span>
-                                </label>
-                                ))}
-                            </div>                        
-                        </div>
-                        <label className="block">
-                            <span className="font-medium">Additional Comments</span>
-                            <textarea value={comments} onChange={({target: {value}}) => setComments(value)} rows={3} className={`${inputClass} form-textarea`} placeholder="Anything extra you would like us to know?"/>
-                        </label>
                         <label className="block">
                             <span className="font-medium">Name</span><span className="text-red-500 pl-1">*</span>
                             <input type="text" value={name} placeholder="e.g. John Smith" onChange={({target: {value}}) => setName(value)} className={`${inputClass}`} required/>
                         </label>
-                        <label className="block">
-                            <span className="font-medium">Email</span><span className={`text-red-500 pl-1 ${emailRequired? "" : "hidden"}`}>*</span>
-                            <input type="email" value={email} placeholder="e.g. John.Smith@email.com" onChange={({target: {value}}) => setEmail(value)} className={`${inputClass}`} required={emailRequired}/>
-                        </label>
-                        <label className="block">
-                            <span className="font-medium">Phone Number</span><span className={`text-red-500 pl-1 ${emailRequired? "hidden" : ""}`}>*</span>
-                            <input type="tel" value={tel} placeholder="e.g. (416) 666-6666" onChange={({target: {value}}) => setTel(value)} className={`${inputClass}`} required={!emailRequired}/>
-                        </label>
-                        <label className="block">
-                            <span className="font-medium">Preferred Contact Method</span>
-                            <select value={pref} onChange={({target: {value}}) => setPref(value)} className={`${inputClass} form-select`}>
-                                <option value="email">Email</option>
-                                <option value="call">Phone Call</option>
-                                <option value="text">Text</option>
-                            </select>
-                        </label>
+                        <div className="grid grid-cols-4 gap-2 desktop:gap-4">
+                            <label className="block col-span-4">
+                                <span className="font-medium">Email</span><span className={`text-red-500 pl-1 ${emailRequired? "" : "hidden"}`}>*</span>
+                                <input type="email" value={email} placeholder="e.g. John.Smith@email.com" onChange={({target: {value}}) => setEmail(value)} className={`${inputClass}`} required={emailRequired}/>
+                            </label>
+                            <label className="block col-span-4 laptop:col-span-2">
+                                <span className="font-medium">Phone Number</span><span className={`text-red-500 pl-1 ${emailRequired? "hidden" : ""}`}>*</span>
+                                <input type="tel" value={tel} placeholder="e.g. (416) 666-6666" onChange={({target: {value}}) => setTel(value)} className={`${inputClass}`} required={!emailRequired}/>
+                            </label>
+                            <label className="block col-span-4 laptop:col-span-2">
+                                <span className="font-medium">Preferred Contact Method</span>
+                                <select value={pref} onChange={({target: {value}}) => setPref(value)} className={`${inputClass} form-select`}>
+                                    <option value="email">Email</option>
+                                    <option value="call">Phone Call</option>
+                                    <option value="text">Text</option>
+                                </select>
+                            </label>
+                        </div>
                         <label className="block cursor-pointer">
                             <input type="checkbox" checked={subscribe} onChange={({target: {checked}}) => setSubscribe(checked)} className={`${inputClass} ${checkClass} mb-1 mr-2`}/>
                             <span className="align-middle ">Subscribe to Brokerage and Real Estate News</span>
                         </label>
-                        <input type="Submit" value="Submit" onChange={()=>{}} className="primary-btn w-full" />
+                        <input type="Submit" value="Submit" onChange={()=>{}} className="primary-btn w-full mt-6" />
                     </fieldset>
                 </form>
             </div>
         </div>
-    </div>)
+    </section>)
 }
 
 export default LeadForm
