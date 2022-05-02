ae49181ed6a29068326217802b204449219f1035
Diadem Shoukralla <diademshoukralla@gmail.com>
2022-05-02 17:44:20 -0400

    fixed the mobile overlay of the menu

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import Menu from "./menu"
 export default function Header({path}) {
   return (
     <header className="outer-layout bg-secondary-50 sticky top-0 z-30 mb-0">
-      <nav className="flex justify-between py-5">
+      <nav className="flex justify-between py-5 relative">
         <Logo
           gaTag="Header"
           className="text-secondary-900 z-30 flex-auto w-1/3 gap-4"
