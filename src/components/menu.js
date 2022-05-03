a521b7847272e878b62e7974b78b82bf06c240d5
Diadem Shoukralla <diademshoukralla@gmail.com>
2022-05-02 20:48:22 -0400

    fixed mobile view of mene: when invisible causes document to overflow. fix was to also update height and width to 0

--- src/components/menu.js
+++ src/components/menu.js
@@ -52,11 +52,11 @@ export default function Menu({className, path}) {
           <MenuIcon className="w-6 h-6 m-auto" />
         )}
       </button>
-      <div className={`bg-black ${isOpen? "opacity-30" : "opacity-0 pointer-events-none"} w-full h-screen overflow-hidden absolute right-0 bottom-0 translate-y-full tablet:hidden transition-opacity duration-300 ease-in-out`}></div>
+      <div className={`bg-black ${isOpen? "opacity-30 h-screen" : "opacity-0 h-0 pointer-events-none"} w-full absolute right-0 bottom-0 translate-y-full tablet:hidden transition-opacity duration-300 ease-in-out`}></div>
       <div
-        className={`bg-secondary-50 w-2/3 tablet:w-full h-screen z-0 ${
-          isOpen ? "translate-x-0" : "translate-x-full tablet:translate-x-0"
-        } outer-layout absolute flex flex-col right-0 bottom-0 pt-10 translate-y-full tablet:translate-y-0 tablet:w-auto tablet:h-auto tablet:m-0 tablet:p-0 tablet:flex tablet:static tablet:flex-row-reverse tablet:flex-nowrap tablet:justify-start tablet:items-center tablet:bg-transparent gap-6 laptop:gap-10 transition-transform tablet:transition-none duration-300 ease-in-out`}
+        className={`bg-secondary-50 tablet:w-full z-0 ${
+          isOpen ? "translate-x-0 w-2/3 h-screen" : "translate-x-full tablet:translate-x-0 h-0 w-0"
+        } outer-layout absolute flex flex-col right-0 bottom-0 pt-10 translate-y-full tablet:translate-y-0 tablet:w-auto tablet:h-auto tablet:m-0 tablet:p-0 tablet:flex tablet:static tablet:flex-row-reverse tablet:flex-nowrap tablet:justify-start tablet:items-center tablet:bg-transparent gap-6 laptop:gap-10 transition-transform tablet:transition-none duration-300 ease-in-out overflow-hidden`}
       >
         {menuData.entries.map(item => (
           <InternalLink
