b2cfa507bf1eb7f5b54e6130bba4e24e90b1cbb8
Diadem Shoukralla <diademshoukralla@gmail.com>
2022-05-10 17:45:39 -0400

    fixed the menu. transitions now work. menu coming from top; not full screen

--- src/components/menu.js
+++ src/components/menu.js
@@ -48,17 +48,19 @@ export default function Menu({className, path}) {
           <MenuIcon className="w-6 h-6 m-auto" />
         )}
       </button>
-      <div className={`bg-black ${isOpen? "opacity-30" : "hidden pointer-events-none"}  h-screen w-full absolute right-0 bottom-0 translate-y-full tablet:hidden`}></div>
+      <div className="pointer-events-none fixed left-0 top-0 overflow-hidden h-screen w-full">
+        <div className={`bg-black ${isOpen? "opacity-[.6] pointer-events-auto " : "opacity-0 pointer-events-none"} h-screen w-full absolute left-0 bottom-0 translate-y-48 tablet:hidden transition-opacity ease-out`}></div>
+      </div>
       <div
-        className={`bg-secondary-50 tablet:w-full w-2/3 h-screen ${
-          isOpen ? " " : "hidden"
+        className={`bg-secondary-50 w-full h-fit pb-12 transition-opacity ease-out ${
+          isOpen ? "opacity-100" : "opacity-0"
         } outer-layout absolute flex flex-col right-0 bottom-0 pt-10 translate-y-full tablet:translate-y-0 tablet:w-auto tablet:h-auto tablet:m-0 tablet:p-0 tablet:flex tablet:static tablet:flex-row-reverse tablet:flex-nowrap tablet:justify-start tablet:items-center tablet:bg-transparent gap-6 laptop:gap-10 overflow-hidden`}
       >
         {menuData.entries.map((item, i) => (
           <InternalLink
             key={i}
             to={item.loc}
-            className={`${item.loc === path? "selected" : ""} block text-left ml-2 tablet:w-auto tablet:text-center tablet:p-0 tablet:m-0 hover:text-accent w-full py-2 font-semibold tablet:border-none border-secondary-400 border-l-2 hover:border-primary-500 duration-75`}
+            className={`${item.loc === path? "selected" : ""} ${isOpen? "" : "-translate-x-full"} transition-transform duration-200 ease-in-out text-left ml-2 tablet:w-auto tablet:text-center tablet:p-0 tablet:m-0 hover:text-accent w-full py-2 font-semibold tablet:border-none border-secondary-400 border-l-2 hover:border-primary-500`}
             label={item.title}
             tag={`Menu > ${item.title}`}
           >
